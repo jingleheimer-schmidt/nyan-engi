a8cba695581cb80c3eb6cdab7374013f8fb78a2b
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2022-01-08 23:44:27 -0800

    store settings in global and update on changed event

--- control.lua
+++ control.lua
@@ -10,21 +10,21 @@ local speeds = {
 }
 
 local pallette = {
-  pastel = {amplitude = 55, center = 200},
-  light = {amplitude = 66, center = 166},
-  default = {amplitude = 127, center = 127},
-  vibrant = {amplitude = 77, center = 77},
-  deep = {amplitude = 55, center = 55},
+  pastel = {amplitude = 55, center = 200},      -- pastel
+  light = {amplitude = 66, center = 166},       -- light
+  default = {amplitude = 127, center = 127},    -- default
+  vibrant = {amplitude = 77, center = 77},      -- neon
+  deep = {amplitude = 55, center = 55},         -- nyan
 }
 
-function make_rainbow(rainbow, game_tick, all_settings)
+function make_rainbow(rainbow, game_tick, settings)
   local index = rainbow.player_index
   local created_tick = rainbow.tick
   -- local game_tick = game.tick
-  local player_settings = all_settings[index]
-  local frequency = speeds[player_settings["nyan-rainbow-speed"].value]
+  local player_settings = settings[index]
+  local frequency = speeds[player_settings["nyan-rainbow-speed"]]
   -- local frequency = 0.050
-  if player_settings["nyan-rainbow-sync"].value == true then
+  if player_settings["nyan-rainbow-sync"] == true then
     created_tick = index
   end
   -- if false == true then
@@ -32,7 +32,7 @@ function make_rainbow(rainbow, game_tick, all_settings)
   -- end
   local pi_3 = 1.0471975512 --[[or math.pi/3]]
   local modifier = (game_tick)+(index*created_tick)
-  local palette_key = player_settings["nyan-rainbow-palette"].value
+  local palette_key = player_settings["nyan-rainbow-palette"]
   -- local palette_key = "default"
   local amplitude = pallette[palette_key].amplitude
   local center = pallette[palette_key].center
@@ -47,13 +47,45 @@ function make_rainbow(rainbow, game_tick, all_settings)
   -- return rainbow_color
 end
 
+script.on_event(defines.events.on_runtime_mod_setting_changed, function(event)
+  local player_index = event.player_index
+  local setting_name = event.setting
+  global.settings[player_index][setting_name] = settings.get_player_settings(player_index)[setting_name].value
+end)
+
+local function initialize_settings()
+  if not global.settings then
+    global.settings = {}
+  end
+  for index, player in pairs(game.players) do
+    local player_settings = settings.get_player_settings(player.index)
+    global.settings[index] = {}
+    global.settings[index]["nyan-rainbow-glow"] = player_settings["nyan-rainbow-glow"].value
+    global.settings[index]["nyan-rainbow-color"] = player_settings["nyan-rainbow-color"].value
+    global.settings[index]["nyan-rainbow-length"] = player_settings["nyan-rainbow-length"].value
+    global.settings[index]["nyan-rainbow-scale"] = player_settings["nyan-rainbow-scale"].value
+    global.settings[index]["nyan-rainbow-speed"] = player_settings["nyan-rainbow-speed"].value
+    global.settings[index]["nyan-rainbow-sync"] = player_settings["nyan-rainbow-sync"].value
+    global.settings[index]["nyan-rainbow-palette"] = player_settings["nyan-rainbow-palette"].value
+  end
+  game.print(serpent.block(global.settings))
+end
+
+script.on_init(function()
+  initialize_settings()
+end)
+
+script.on_configuration_changed(function()
+  initialize_settings()
+end)
+
 script.on_event(defines.events.on_player_changed_position, function(event)
   local player_index = event.player_index
-  local all_settings = {}
-  all_settings[player_index] = settings.get_player_settings(player_index)
-  local player_settings = all_settings[player_index]
-  local sprite = player_settings["nyan-rainbow-color"].value
-  local light = player_settings["nyan-rainbow-glow"].value
+  local settings = global.settings
+  -- all_settings[player_index] = settings.get_player_settings(player_index)
+  local player_settings = settings[player_index]
+  local sprite = player_settings["nyan-rainbow-color"]
+  local light = player_settings["nyan-rainbow-glow"]
   local player = {}
   if sprite or light then
     player = game.get_player(player_index)
@@ -61,8 +93,8 @@ script.on_event(defines.events.on_player_changed_position, function(event)
     return
   end
   local event_tick = event.tick
-  local length = tonumber(player_settings["nyan-rainbow-length"].value)
-  local scale = tonumber(player_settings["nyan-rainbow-scale"].value)
+  local length = tonumber(player_settings["nyan-rainbow-length"])
+  local scale = tonumber(player_settings["nyan-rainbow-scale"])
   if sprite then
     sprite = rendering.draw_sprite{
       sprite = "nyan",
@@ -89,7 +121,7 @@ script.on_event(defines.events.on_player_changed_position, function(event)
       -- visible = {sprite = false, light = false}
     }
     global.sprites[sprite] = sprite_data
-    local rainbow_color = make_rainbow(sprite_data, event_tick, all_settings)
+    local rainbow_color = make_rainbow(sprite_data, event_tick, settings)
     -- local rainbow_color = {1,1,1,1}
     rendering.set_color(sprite, rainbow_color)
     -- rendering.bring_to_front(sprite)
@@ -120,7 +152,7 @@ script.on_event(defines.events.on_player_changed_position, function(event)
       -- visible = {sprite = false, light = false}
     }
     global.lights[light] = light_data
-    local rainbow_color = make_rainbow(light_data, event_tick, all_settings)
+    local rainbow_color = make_rainbow(light_data, event_tick, settings)
     -- local rainbow_color = {1,1,1,1}
     rendering.set_color(light, rainbow_color)
   end
@@ -133,11 +165,7 @@ script.on_event(defines.events.on_tick, function(event)
     return
   end
   local game_tick = event.tick
-  local all_settings = {}
-  for _, player in pairs(game.connected_players) do
-    local index = player.index
-    all_settings[index] = settings.get_player_settings(index)
-  end
+  local settings = global.settings
   for _, id in pairs(render_ids) do
     local rainbow = global.sprites[id] or global.lights[id]
     if rainbow then
@@ -150,7 +178,7 @@ script.on_event(defines.events.on_tick, function(event)
           global.lights[id] = nil
         end
       else
-        local rainbow_color = make_rainbow(rainbow, game_tick, all_settings)
+        local rainbow_color = make_rainbow(rainbow, game_tick, settings)
         -- local rainbow_color = {1,1,1,1}
         -- local size = rainbow.size
         -- local scale = rainbow.scale
